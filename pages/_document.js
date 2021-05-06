571cbd8c642888601b808b0ea8cd1a4621e4af68
Yakup Garayev <yakup.garayev@bellevuecollege.edu>
2021-05-06 02:42:09 -0700

    add some description

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
         <title>Baab World</title>
-        <meta name="description" content="Brand New Sneakers that you could find in a perfect condition and for a very suitable price" />
+        <meta name="description" content="Brand New Sneakers that you could find in a perfect condition and for a very suitable price!" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
